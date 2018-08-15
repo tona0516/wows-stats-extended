bb43e56910ebba42db437bb2a12c1659c2d47a86
tona0516 <tona.meem@gmail.com>
2018-08-16 05:47:02 +0900

    feat: 必要情報の送信部分を作成

--- routes/install.js
+++ routes/install.js
@@ -1,9 +1,37 @@
+const log4js = require('log4js');
+const dotenv = require('dotenv');
 var express = require('express');
 var router = express.Router();
 
+const logger = log4js.getLogger();
+logger.level = 'DEBUG';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('install', { title: 'INSTALLATION' });
+    res.render('install', { title: 'インストール', isValid: null});
 });
+
+router.post('/', function(req, res, next) {
+    const isValid =  validateParameter(req.body) ? true : false;
+    const message = isValid ? "登録完了" : "パラメータが無効です";
+
+    res.render('install', { title: 'インストール', isValid: isValid, message: message});
+});
+
+const validateParameter = function(parameter) {
+    const appid = parameter.appid;
+    const region = parameter.region;
+    const directory = parameter.directory;
+
+    return true;
+}
+
+const validateAppID = function(appid) {
+
+}
+
+const validateInstallDirectory = function(directory) {
+
+}
 
 module.exports = router;
