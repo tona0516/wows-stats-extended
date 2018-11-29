24b22d0bb7cab15a262d1ed37e8b28d137f995bd
tona0516 <tonango.0516@gmail.com>
2018-11-30 00:51:49 +0900

    refactor: クライアントのJSを整理

--- public/javascripts/vue.js
+++ public/javascripts/vue.js
@@ -7,36 +7,62 @@ var app = new Vue({
 })
 
 const DOMAIN = 'http://localhost:3000';
+const FETCH_INTERVAL_MS = 1000;
 var isFetching = false;
 var isFirstFetch = true;
 
-const fetchShipConcealment = function() {
-  return new Promise((resolve) => {
-    var request = new XMLHttpRequest();
-    request.open("GET", DOMAIN + '/external_api/info/ship_concealment');
-    request.addEventListener("load", (event) => {
-      resolve();
-    });
-    request.send();
-  })
+const Status = {
+  NEED_NOT_FETCH : 1,
+  FETCHING       : 2,
+  FETCH_FAIL     : 3,
+  FETCH_SUCCESS  : 4,
 }
 
-const checkUpdate = function() {
+/**
+ * 共通リクエストメソッド
+ * 
+ * @param {String} url 
+ * @param {Function} didLoad 
+ * @param {String} method 
+ */
+const requestCommon = function(url, didLoad, method = 'GET') {
+  const request = new XMLHttpRequest();
+  request.open(method, url);
+  request.addEventListener("load", (event) => {
+    didLoad(event);
+  });
+  request.send();
+}
+
+/**
+ * 開発用
+ * 艦の隠蔽距離を公式サイトから取得する
+ */
+const fetchShipConcealment = function() {
   return new Promise((resolve) => {
-    var request = new XMLHttpRequest();
-    request.open("GET", DOMAIN + '/external_api/check_update');
-    request.addEventListener("load", (event) => {
-      resolve(event.target.status);
+    requestCommon(DOMAIN + '/external_api/info/ship_concealment', (event) => {
+      resolve();
     });
-    request.send();
   });
 }
 
+/**
+ * 新しい戦闘が始まったかをチェックする
+ */
+const checkUpdate = function() {
+  return new Promise((resolve) => {
+    requestCommon(DOMAIN + '/external_api/check_update', (event) => {
+      resolve(event.target.status);
+    });
+  });
+}
+
+/**
+ * 銭湯データを取得する
+ */
 const fetch = function() {
   return new Promise((resolve, reject) => {
-    var request = new XMLHttpRequest();
-    request.open("GET", DOMAIN + '/external_api/fetch');
-    request.addEventListener("load", (event) => {
+    requestCommon(DOMAIN + '/external_api/fetch', (event) => {
       const statusCode = event.target.status;
       const responseBody = event.target.responseText;
       const fetchedData = JSON.parse(responseBody);
@@ -50,10 +76,44 @@ const fetch = function() {
         return resolve();
       }
     });
-    request.send();
   });
 }
 
+/**
+ * 状態に応じてViewと変数を更新する
+ * 
+ * @param {Status} status 
+ */
+const updateStatus = function(status, error = null) {
+  if (status === Status.NEED_NOT_FETCH) {
+    app.message = '現在戦闘中ではありません。戦闘開始時に自動更新します。';
+    return;
+  }
+
+  if (status === Status.FETCHING) {
+    isFetching = true;
+    app.message = "読み込み中...";
+    return;
+  }
+
+  if (status === Status.FETCH_FAIL) {
+    isFetching = false;
+    isFirstFetch = false;
+    app.message = "読み込みに失敗しました。もう一度お試しください: " + error;
+    return;
+  }
+
+  if (status === Status.FETCH_SUCCESS) {
+    isFetching = false;
+    isFirstFetch = false;
+    app.message = null;
+    return;
+  }
+}
+
+/**
+ * 更新があるときに銭湯データを取得し反映する
+ */
 const fetchIfNeeded = async function() {
   // fetch中の時は新たにfetchしない
   if (isFetching) {
@@ -65,28 +125,23 @@ const fetchIfNeeded = async function() {
 
   // 戦闘中でない場合
   if (status == 299) {
-    app.message = '現在戦闘中ではありません。戦闘開始時に自動更新します。';
+    updateStatus(Status.NEED_NOT_FETCH);
     return;
   }
 
   // 新しい戦闘が開始された、もしくはユーザが画面をリロードした場合
   if (status == 200 || isFirstFetch) {
-    isFetching = true;
-    app.message = "読み込み中...";
+    updateStatus(Status.FETCHING);
 
     await fetch().catch((error) => {
-      isFetching = false;
-      isFirstFetch = false;
-      app.message = "読み込みに失敗しました。もう一度お試しください: " + error;
+      updateStatus(Status.FETCH_FAIL, error);
       return;
     });
 
-    isFetching = false;
-    isFirstFetch = false;
-    app.message = null;
+    updateStatus(Status.FETCH_SUCCESS);
     return;
   }
 }
 
 // fetchShipConcealment();
-setInterval(fetchIfNeeded, 1000);
+setInterval(fetchIfNeeded, FETCH_INTERVAL_MS);
