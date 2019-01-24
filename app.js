ce1e4568c845b2ec049c073e4c56c8b70b9b5a7e
tona0516 <tonango.0516@gmail.com>
2019-01-25 00:27:32 +0900

    developmentのときのみaccessログを出力するよう修正

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+if (app.get('env') === 'development') {
+  app.use(logger('dev'));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
