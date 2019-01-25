32c12f8025921676152f8fff9a29fb8adaf564e8
tona0516 <tona.meem@gmail.com>
2019-01-25 17:19:03 +0900

    fix: Windowsで起動しない問題を修正

--- install.bat
+++ install.bat
@@ -1,1 +1,2 @@
+set NODE_ENV=production
 npm install && start "" http://localhost:3000/install && npm run start-production
--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
+set NODE_ENV=production
 start "" http://localhost:3000 && npm run start-production
