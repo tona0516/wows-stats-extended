2de53c740eac0ce0bcb8c0c86c47557adf116f8f
tona0516 <tonango.0516@gmail.com>
2021-08-29 03:48:36 +0900

    Fix node env

--- src/dependency_injection.ts
+++ src/dependency_injection.ts
@@ -36,7 +36,8 @@ export class DependencyInjection {
   }
 
   private init(): TsyringeDependencyContainer {
-    const logLevel = process.env.NODE_ENV === "release" ? "info" : "debug";
+    const logLevel =
+      process.env.NODE_ENV === "production" ? "info" : "debug";
 
     container.register("LogLevel", { useValue: logLevel });
     container.register("Logger", { useClass: Logger });
