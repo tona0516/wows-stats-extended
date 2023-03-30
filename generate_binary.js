66d79bd8f2ad8b83dcadf471f45a45aa84bcf312
tona0516 <tonango.0516@gmail.com>
2023-03-30 16:17:11 +0900

    Fix style

--- generate_binary.js
+++ generate_binary.js
@@ -1,7 +1,15 @@
-const { exec } = require('pkg')
+const { exec } = require("pkg");
 const packageJsonPath = "./package.json";
 const packageJson = require(packageJsonPath);
 
 (async () => {
-    await exec(["dist/app.js", "--config", packageJsonPath, "--output", `${packageJson.name}-${packageJson.version}`, "--compress", "Brotli"])
-})()
+  await exec([
+    "dist/app.js",
+    "--config",
+    packageJsonPath,
+    "--output",
+    `${packageJson.name}-${packageJson.version}`,
+    "--compress",
+    "Brotli",
+  ]);
+})();
